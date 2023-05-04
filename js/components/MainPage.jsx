00af9dc29fe59369279daa8f8cd3c57b939ec41d
Adrian <cioban.adrian10@gmail.com>
2023-05-04 22:21:36 +0300

    afisare date in mainpage

--- js/components/MainPage.jsx
+++ js/components/MainPage.jsx
@@ -1,4 +1,38 @@
+import { useEffect, useState } from "react";
+
 // js/components/MainPage.jsx
 export default function MainPage() {
-  return <div>Aceasta este pagina principala</div>;
+  const [records, setRecords] = useState([]);
+
+  useEffect(() => {
+    try {
+      fetch("/api/records", {
+        method: "GET",
+      })
+        .then((response) => response.json())
+        .then((json) => setRecords(json.data));
+    } catch (e) {
+      console.log(e);
+    }
+  }, []);
+
+  console.log(records);
+
+  return (
+    <section>
+      <div>
+        <h1>Fun facts app</h1>
+        <p>This is an app that showcases fun facts</p>
+
+        <div>
+          {records.map((record) => (
+            <div key={record._id}>
+              <h3>{record.titlu}</h3>
+              <p>{record.descriere}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
 }
