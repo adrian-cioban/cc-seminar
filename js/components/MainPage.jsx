e73b0d79e934e29e9fed4cd5c4f2db3ad6b021a2
Adrian <cioban.adrian10@gmail.com>
2023-05-05 17:02:19 +0300

    insert page & delete button

--- js/components/MainPage.jsx
+++ js/components/MainPage.jsx
@@ -19,16 +19,32 @@ export default function MainPage() {
   console.log(records);
 
   return (
-    <section>
-      <div>
-        <h1>Fun facts app</h1>
-        <p>This is an app that showcases fun facts</p>
+    <section className="bg-white">
+      <div className="container px-6 py-10 mx-auto">
+        <h1 className="w-[500px] mx-auto text-center text-6xl font-bold text-blue-600">
+          Fun facts app
+        </h1>
+        <p className="w-[1000px] mx-auto text-center mt-4 text-3xl text-blue-600">
+          This is an app that showcases fun facts
+        </p>
 
-        <div>
+        <div className="grid grid-cols-1 gap-8 mt-8 xl:mt-12 xl:gap-12 sm:grid-cols-2 xl:grid-cols-4 lg:grid-cols-3">
           {records.map((record) => (
-            <div key={record._id}>
-              <h3>{record.titlu}</h3>
-              <p>{record.descriere}</p>
+            <div
+              key={record._id}
+              className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow"
+            >
+              <h3 className="mb-2 text-2xl font-bold text-red-500">
+                {record.titlu}
+              </h3>
+              <p className="font-normal">{record.descriere}</p>
+
+              <button
+                type="button"
+                class="mt-4 text-gray-900 bg-gradient-to-r from-red-200 via-red-300 to-yellow-200 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-red-100 dark:focus:ring-red-400 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+              >
+                Delete
+              </button>
             </div>
           ))}
         </div>
